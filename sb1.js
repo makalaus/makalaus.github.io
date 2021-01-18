6727697f29b48efb964e39fe12aca884d99cabf7
makalaus <33875665+makalaus@users.noreply.github.com>
2021-01-18 10:45:22 +0100

    Update sb1.js

--- sb1.js
+++ sb1.js
@@ -1,3 +1,3 @@
-var phone = document.getElementById('phone');
+var phone = document.getElementById('phone').innerText;
 console.log(phone);
-phone.innerHTML.replace(/ /g, '');
+phone.replace(/ /g, '');
